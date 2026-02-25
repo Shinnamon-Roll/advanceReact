a8072509d3f66b2bbcf948d696caffe969d4d5de
-Shinnamon- <chimonkung0@gmail.com>
2026-02-25 09:17:54 +0700

    feat: add contact component and dynamic hello list
    - Import Contact component and render it with static props
    - Replace static Hello component with dynamic list rendering from helloData array
    - Add data array to demonstrate component reuse

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import Contact from './assets/contact.jsx'
 import Hello from './assets/hello.jsx'
 
 function App() {
+  const helloData = [
+    { name: 'Shinnnamon', message : 'Hi there'},
+    { name: 'Tom', message : 'Hello World'},
+  ];
+
   return (
-    <>
-      <div>
-        <Hello />
-      </div>
-    </>
-  )
+    <div >
+      {helloData.map((item, index) => (
+        <Hello key={index} name={item.name} message={item.message} />
+      ))}
+
+      <Contact email="chimonkung0@gmail.com" phone="0810154373"/>
+    </div>
+  );
 }
 
 export default App
